5ea8c1b2b408e25b7aa0ac7958ae2a7f9ff3e314
vladislav321 <36309664+vladislav321@users.noreply.github.com>
2022-10-01 14:02:37 +0300

    init

---
+++ modules/auth/service.ts
@@ -0,0 +1,19 @@
+import { User } from './dto/user';
+
+class AuthService
+{
+    /**
+     *
+     */
+    constructor() {
+        
+    }
+
+    getUsers(): void{
+        
+    }
+
+    getUserById(id: string) : User{
+        return new User();
+    }
+}
